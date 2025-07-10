191e662d09a4595ab41ebcdc0aed22578c5230e4
Simarjeet Kaur <simarjeetkaur6603@gmail.com>
2025-07-11 00:28:15 +0530

    feed view ui setup

---
+++ Threads Clone/Core/Feed/View/ThreadCell.swift
@@ -0,0 +1,88 @@
+//
+//  ThreadCell.swift
+//  Threads Clone
+//
+//  Created by Simarjeet Kaur on 10/07/25.
+//
+
+import SwiftUI
+
+struct ThreadCell: View {
+    
+    var body: some View {
+        VStack{
+            HStack (alignment: .top ,spacing:12){
+                Image("max-verstappen")
+                    .resizable()
+                    .scaledToFill()
+                    .frame(width: 45, height: 45)
+                    .clipShape(Circle())
+                
+                VStack(alignment: .leading, spacing:4){
+                    HStack{
+                        Text("maxverstappen1")
+                            .font(.footnote)
+                            .fontWeight(.semibold)
+                        
+                        Spacer()
+                        
+                        Text("10m")
+                            .font(.caption)
+                            .foregroundColor(Color(.systemGray3))
+                        
+                        Button {
+                    
+                        } label: {
+                            Image(systemName: "ellipsis")
+                                .foregroundColor(Color(.darkGray))
+                        }
+
+                    }
+                    
+                    Text("Formula 1 champion")
+                        .font(.footnote)
+                        .multilineTextAlignment(.leading)
+                    
+                    HStackLayout(spacing:16){
+                        Button {
+                        
+                        } label: {
+                            Image(systemName: "heart")
+                        }
+                        
+                        Button {
+                        
+                        } label: {
+                            Image(systemName: "bubble.right")
+                        }
+
+                        
+                        Button {
+                        
+                        } label: {
+                            Image(systemName: "arrow.rectanglepath")
+                        }
+
+                        
+                        Button {
+                        
+                        } label: {
+                            Image(systemName: "paperplane")
+                        }
+
+
+                    }
+                    .foregroundColor(.black )
+                    .padding(.vertical,8)
+                   
+                }
+            }
+            Divider()
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ThreadCell()
+}
